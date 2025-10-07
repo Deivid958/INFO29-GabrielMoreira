955ac2fca7ef275aa47bd04e47005377faa52814
Deivid958 <deividsantos49606@gmail.com>
2025-10-07 14:07:26 -0300

    Update escola.c
    adicionado disciplina no menu

--- projeto_escola/escola.c
+++ projeto_escola/escola.c
@@ -1,78 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "escola.h"
 #define TAM_ALUNO 50
-
-typedef struct
-{
-    char nome[TAM_ALUNO];
-    char matricula[TAM_ALUNO];
-    char sexo;
-    char data[TAM_ALUNO];
-    char cpf[TAM_ALUNO];
-    int ativo;
-} aluno;
-
-char menugeral(void);
-char menu_aluno(void);
-void cadastrar_aluno(int posicao, aluno alunos[]);
-
-int main(void)
-{
-    int sair = 0, sair_aluno = 0;
-    int opcao = 0, opcao_aluno = 0; 
-    aluno alunos[TAM_ALUNO];
-    int q_alunos = 0;
-
-    while (!sair)
-    {
-        opcao = menugeral();
-        sair_aluno = 0;
-
-        switch(opcao)
-        {
-            case '0':{
-                sair = 1;
-                break;
-            }
-            case '1':{
-                while (!sair_aluno)
-                {
-                    opcao_aluno = menu_aluno();
-
-                    switch(opcao_aluno)
-                    {
-                        case '0':
-                        {
-                            sair_aluno = 1;
-                            break;
-                        }
-                        case '1':
-                        {
-                            cadastrar_aluno(q_alunos, alunos);
-                            q_alunos++;
-                        }
-                        
-                    }
-
-                } 
-                break;
-            }
-            
-            case '2':{
-                printf("Professores\n");
-                break; 
-            }
-            case '3':{
-                printf("Relatórios\n");
-                break;
-            }
-            default:{
-                printf("opção invalida!\n");
-                break;
-            }    
-        }
-    }
-}
+#define aluno_inexistente -1
+#include "Disci_funcoes.h"
+#define TAM_ALUNO 50
+#define TAM_DISCIPLINA 10
 
 char menugeral(void)
 {
@@ -82,7 +16,8 @@ char menugeral(void)
     printf("[0] - Sair\n");
     printf("[1] - Alunos\n");
     printf("[2] - Professores\n");
-    printf("[3] - Relatórios\n");
+    printf("[3] - Disciplinas\n");
+    printf("[4] - Relatórios\n");
     printf("----------------------------\n");
     printf("Opção: ");
     resposta = getchar();
